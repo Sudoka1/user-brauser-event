4545b0c93f8c78f6a5c2b0be4f18f90b28cc2108
Sabir <sabir.suleyman@dci-student.org>
2024-04-24 14:20:37 +0200

    user 2

---
+++ userevents-brauserevents.js
@@ -0,0 +1,7 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button wurde geklickt!');
+});
+
+window.addEventListener('load', function() {
+    console.log('Die Seite wurde geladen.');
+});
